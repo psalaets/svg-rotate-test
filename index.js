31676ad2c4988059c3439a52f64a500b9399b251
Paul Salaets <psalaets@gmail.com>
2015-10-18 13:07:01 -0400

    make setting bookmarkable and shareable

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 angular.module('app', [])
-.controller('MainCtrl', function($scope) {
+.controller('MainCtrl', function($scope, $location) {
+  var objectNames = ['group', 'big', 'little'];
+
+  // update scope objects when search changes
+  $scope.$on('$locationChangeSuccess', function() {
+    angular.forEach($location.search(), function(value, key) {
+      var object = key.split('.')[0];
+      var property = key.split('.')[1];
+
+      if (objectNames.indexOf(object) != -1) {
+        $scope[object][property] = Number(value);
+      }
+    });
+  });
+
+  // update search when scope objects change
+  objectNames.forEach(function(objectName) {
+    var deep = true;
+    $scope.$watch(objectName, updateSearchFromObject(objectName), deep);
+  });
+
+  function updateSearchFromObject(objectName) {
+    return function(newValue) {
+      angular.forEach(newValue, function(value, key) {
+        $location.search(objectName + '.' + key, value);
+      });
+    };
+  }
+
   $scope.reset = function() {
     $scope.group = {
-      x: 100,
-      y: 80,
+      x: 200,
+      y: 180,
       rotate: 0,
       rx: 0,
       ry: 0,
@@ -31,4 +59,4 @@ angular.module('app', [])
   };
 
   $scope.reset();
-});
+})
